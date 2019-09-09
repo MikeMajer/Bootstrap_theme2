dc5d0c82f9823d760ec3fb5591ea0da5961f76a2
Mike Majer <majer.michal@outlook.com>
2019-09-09 16:03:33 +1000

    Add Restaurant Menu Section

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // Get the current year for the copyright
-$('#year').text(new Date().getFullYear()); 
+$('#year').text(new Date().getFullYear());
+
+// Random Price Generator
+let prices = [...$(".card-footer h3")];
+
+$.each([...prices], (i, price) => {
+  $(price).text(`$${(Math.floor(Math.random() * 40 + 10))}`);
+  console.log(price)
+});
+
