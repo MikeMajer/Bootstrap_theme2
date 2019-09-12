fec7c459960e8ba7643708c351bc3cab641853be
Mike Majer <majer.michal@outlook.com>
2019-09-12 12:26:30 +1000

    Add Scrollspy and Smooth Scrolling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
 
+
 // Random Price Generator
 let prices = [...$(".card-footer h3")];
 
@@ -8,8 +9,8 @@ $.each([...prices], (i, price) => {
   $(price).text(`$${(Math.floor(Math.random() * 40 + 10))}`);
 });
 
-// Navbar scroll background
 
+// Navbar scroll background change
 $(window).scroll(() => {
   const nav = $("#main-nav");
   const mediumScreenSize = 768;
@@ -24,8 +25,101 @@ $(window).scroll(() => {
 });
 
 // Lightbox init
-
 $(document).on('click', '[data-toggle="lightbox"]', function (event) {
   event.preventDefault();
   $(this).ekkoLightbox();
+});
+
+
+// Scroll Spy init
+$('body').scrollspy({ target: '#main-nav' });
+
+
+// Smooth Scrolling
+$("#main-nav a").on('click', function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html,body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function () {
+
+      window.location.hash = hash;
+    });
+  }
+});
+
+
+
+// Restaurant Menu
+
+function dropdownClassRemove() {
+  $(".dropdown-item").removeClass("active");
+}
+
+// Menu section tab navigation
+$("#menuTab a").on("click", function () {
+  if (this.text !== $(".dropdown-item.active").text()) {
+    dropdownClassRemove()
+    if (this.text === "Breakfast") {
+      $(".dropdown-item:nth-of-type(1)").addClass("active");
+    } else if (this.text === "Lunch") {
+      $(".dropdown-item:nth-of-type(2)").addClass("active");
+    } else if (this.text === "Dinner") {
+      $(".dropdown-item:nth-of-type(3)").addClass("active");
+    } else if (this.text === "Mains") {
+      $(".dropdown-item:nth-of-type(4)").addClass("active");
+    }
+  }
+})
+
+// Dropdown List - navbar
+$(".dropdown-item").on("click", function (e) {
+
+  const tab = this.text;
+
+  dropdownClassRemove();
+  $("#menuTabContent .tab-pane").removeClass("show active");
+  $("#menuTab .nav-link").removeClass("active");
+  $(this).addClass("active");
+
+
+  if (tab === "Breakfast") {
+    $("#breakfast-tab").addClass("active");
+    $("#menuTabContent #breakfast").addClass("active show");
+  } else if (tab === "Lunch") {
+    $("#lunch-tab").addClass("active");
+    $("#menuTabContent #lunch").addClass("active show");
+  } else if (tab === "Dinner") {
+    $("#dinner-tab").addClass("active");
+    $("#menuTabContent #dinner").addClass("active show");
+  } else if (tab === "Mains") {
+    $("#mains-tab").addClass("active");
+    $("#menuTabContent #mains").addClass("active show");
+  }
+
+});
+
+
+// Highlight active tab in navbar when scrolling
+$(window).on('activate.bs.scrollspy', function (e, obj) {
+
+  const section = obj.relatedTarget;
+
+  if (section === "#menu") {
+    dropdownClassRemove();
+
+    if (($("#menuTabContent #breakfast").hasClass("show"))) {
+      $(".dropdown-item:nth-of-type(1)").addClass("active")
+    } else if (($("#menuTabContent #lunch").hasClass("show"))) {
+      $(".dropdown-item:nth-of-type(2)").addClass("active")
+    } else if (($("#menuTabContent #dinner").hasClass("show"))) {
+      $(".dropdown-item:nth-of-type(3)").addClass("active")
+    } else if (($("#menuTabContent #mains").hasClass("show"))) {
+      $(".dropdown-item:nth-of-type(4)").addClass("active")
+    }
+  }
+
 });
